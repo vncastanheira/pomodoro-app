6224a62c26407a4c407bf735071fec53696c27a6
Vinicius <vncastanheira@gmail.com>
2019-04-08 15:08:31 -0300

    Updates

---
+++ precache-manifest.b7bfaa33f1f29587a8f3c9023ad537c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "672374e650102f61ec17",
+    "url": "/pomodoro-app/static/css/main.99d40291.chunk.css"
+  },
+  {
+    "revision": "672374e650102f61ec17",
+    "url": "/pomodoro-app/static/js/main.a7f4e890.chunk.js"
+  },
+  {
+    "revision": "d846992fb9918d3e4ac7",
+    "url": "/pomodoro-app/static/js/runtime~main.fcbfb025.js"
+  },
+  {
+    "revision": "94f72e92a9fcc70b60a8",
+    "url": "/pomodoro-app/static/js/2.8d7116ab.chunk.js"
+  },
+  {
+    "revision": "0c95a6aa6313451a4f8d254b04ab245e",
+    "url": "/pomodoro-app/static/media/monofonto.0c95a6aa.ttf"
+  },
+  {
+    "revision": "7caf5e91fbd727c92aa8bf0930056678",
+    "url": "/pomodoro-app/index.html"
+  }
+];
